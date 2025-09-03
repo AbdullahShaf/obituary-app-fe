fb042f943d01c7eff15b86916d10d66802e3c6fd
uzair888 <uzairsarwar10@gmail.com>
2025-09-03 22:23:44 +0500

    Integrate popups according to figma

--- app/components/appcomponents/InfoModal.js
+++ app/components/appcomponents/InfoModal.js
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { X, Pencil } from "lucide-react";
 
 export default function InfoModal({ heading, text, name, icon }) {
   const [isOpen, setIsOpen] = useState(true);
@@ -22,22 +21,22 @@ export default function InfoModal({ heading, text, name, icon }) {
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-sm">
-      <div className="relative mobile:w-[70%] w-[460px] max-w-full rounded-xl bg-white shadow-xl px-6 py-12">
+      <div className="relative mobile:w-[70%] w-[460px] max-w-full rounded-xl bg-[#E7EEF3] shadow-xl px-6 py-12">
         {/* Close Button */}
         <button
           onClick={() => setIsOpen(false)}
-          className="absolute right-3 top-3 border-[3px] border-[#6D778E] rounded-full p-1 hover:bg-gray-100"
+          className="absolute right-3 top-3 "
         >
-          <X size={15} color="#6D778E" />
+          <img className="w-10 h-10" src="/close-icon.svg" />
         </button>
 
         {/* Content */}
         <div className="flex items-center justify-center my-3">
-          <div className="flex items-center w-[90%] space-x-3 rounded-[1px] bg-gradient-to-b from-[#0D94E8] to-[#1860A3] p-4 text-white shadow-md">
+          <div className="flex items-center w-[90%] space-x-3 rounded-[1px] border border-[#0D94E8] bg-gradient-to-b from-[#0D94E8] to-[#1860A3] p-4 text-white shadow-md">
             <img className="w-9 h-9" src={icon} />
-            <div className="flex flex-col text-[#FFFFFF] items-start">
-              <h2 className="font-bold">{heading}</h2>
-              <p>{text}</p>
+            <div className="flex flex-col text-[#B2E6E3] items-start">
+              <h2>{heading}</h2>
+              <p className="text-[#FFFFFF]">{text}</p>
               <p>{name}</p>
             </div>
           </div>
