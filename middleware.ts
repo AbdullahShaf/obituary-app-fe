abf3ec8af19f90798ec68c40f62d858f19a52206
mhaseeb-0 <206386957+mhaseebbasharat@users.noreply.github.com>
2025-09-08 12:57:36 +0500

    Revert "Changes by Haseeb"
    This reverts commit 9ca3c845e5f6bba709456c6f90b38a20230ee5a8.

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-const PUBLIC_ROUTES = ["/", "/registracija", "/auth/signin", "/auth/error", "/osmrtnice-vpis"];
+const PUBLIC_ROUTES = ["/", "/registracija", "/auth/signin", "/auth/error"];
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
