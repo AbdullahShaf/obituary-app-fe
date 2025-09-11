7f8d0ec26ec2a5abe78a2b8000fb733252fa614c
mm2439 <gamspob@yahoo.com>
2025-09-11 13:44:47 +0200

    Revert "date changes in cards"

--- app/osmrtnice-vpis/page.js
+++ app/osmrtnice-vpis/page.js
@@ -24,11 +24,21 @@ const Obituaryform = () => {
     }
 
 
+    // Temporarily commented
+    // Check if user has permission to create obituaries
     if (!isLoading && !isAuthenticated && !user.createObituaryPermission) {
       toast.error("You don't have permission to create obituaries.");
       router.push("/");
       return;
     }
+
+   
+    if (!isLoading && !isAuthenticated &&!user.createObituaryPermission) {
+      toast.error("You don't have permission to create obituaries.");
+      router.push("/");
+      return;
+    }
+
 
   }, [isLoading, isAuthenticated, user]);
 
@@ -51,6 +61,8 @@ const Obituaryform = () => {
     );
   }
 
+
+  // Temporarily commented
   if (!user?.createObituaryPermission) {
     return (
       <Layout
@@ -78,6 +90,7 @@ const Obituaryform = () => {
       </Layout>
     );
   }
+
 
   return (
     <Layout
