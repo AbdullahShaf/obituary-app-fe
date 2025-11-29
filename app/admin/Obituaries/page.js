554edec588352455aaaae26cdfed87617365cfbe
Nikhil Jha <shivamjha01000@gmail.com>
2025-11-30 00:24:39 +0530

    New Cemetery attached with Orbit Form

--- app/admin/Obituaries/page.js
+++ app/admin/Obituaries/page.js
@@ -204,7 +204,7 @@ const Obituaries = () => {
     invoiceImg: obituary.deathReportExists ? "/ico_eye.png" : "",
     invoice: formatDate(obituary.funeralTimestamp),
     funeral: formatTime(obituary.funeralTimestamp),
-    cemetery: obituary.funeralCemetery || "Unknown Cemetery",
+    cemetery: obituary.Cemeteries?.name || obituary.Cemetry?.name || "Unknown Cemetery",
     Obituary: obituary.User?.company || obituary.User?.name || "Unknown",
     postedby: obituary.User?.city || "Unknown",
     date: formatDate(obituary.createdTimestamp),
